87e62a417f23bb4c6435765150d88189ec2adde9
Maxim Gorkovskiy <maxim@bryntum.com>
2022-12-29 19:22:47 +0300

    run all examples at once

--- run.ps1
+++ run.ps1
@@ -3,22 +3,35 @@ $demo = $args[1];
 
 if ($args.Count -eq 1) {
     $demo = $lang;
-    $lang = "rust";
+    $lang = "all";
 }
 
-if ($lang -eq "rust") {
-    $path = Resolve-Path -Path "$lang\leet\target\release\leet.exe" -Relative;
+function Invoke-RunRust {
+    $path = Resolve-Path -Path "rust\leet\target\release\leet.exe" -Relative;
     
+    Write-Output("Rust");
     Invoke-Expression "$path $demo"
 }
-elseif ($lang -eq "node") {
-    $path = Resolve-Path -Path "$lang\\index_node.mjs" -Relative;
+
+function Invoke-RunJS {
+    $path = Resolve-Path -Path "node\\index_node.mjs" -Relative;
 
     Write-Output("Node");
     node --experimental-modules $path $demo
 
-    $path = Resolve-Path -Path "$lang\\index_deno.js" -Relative;
+    $path = Resolve-Path -Path "node\\index_deno.js" -Relative;
 
     Write-Output("Deno");
     deno run --allow-hrtime $path $demo
+}
+
+if ($lang -eq "all") {
+    Invoke-RunJS;
+    Invoke-RunRust;
+}
+elseif ($lang -eq "rust") {
+    Invoke-RunRust;
+}
+elseif ($lang -eq "node") {
+    Invoke-RunJS;
 }
